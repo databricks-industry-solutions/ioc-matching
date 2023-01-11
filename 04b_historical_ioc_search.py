9d927d012c548d33a232cb464cf9b7b7c932cd1e
lipyeow-lim <lipyeow-lim>
2023-01-11 01:54:32 +0000

    minor tweaks

--- 04b_historical_ioc_search.py
+++ 04b_historical_ioc_search.py
@@ -28,8 +28,8 @@
 # DBTITLE 1,Design & test out the summarizing scheme (group by attributes)
 # MAGIC %sql
 # MAGIC 
-# MAGIC --select count(*) as num_rows
-# MAGIC --from (
+# MAGIC select count(*) as num_rows
+# MAGIC from (
 # MAGIC 
 # MAGIC   select date_trunc('HOUR', ts::timestamp) as ts_bkt, 
 # MAGIC     `id.orig_h`,
@@ -43,7 +43,7 @@
 # MAGIC   from ioc_matching_lipyeow_lim.http_big
 # MAGIC   group by ts_bkt, `id.orig_h`, `id.resp_h`, method, status_code, user_agent
 # MAGIC 
-# MAGIC --)
+# MAGIC )
 
 # COMMAND ----------
 
@@ -141,7 +141,9 @@
 # MAGIC 
 # MAGIC select *
 # MAGIC from ioc_matching_lipyeow_lim.summary_all
-# MAGIC where obs_value = '192.168.21.253'
+# MAGIC where ts_bkt between '2012-03-16T00:00:00.000+0000' and '2012-03-17T00:00:00.000+0000' 
+# MAGIC   and obs_value = '192.168.21.253'
+# MAGIC order by cnt
 
 # COMMAND ----------
 
@@ -150,8 +152,8 @@
 # MAGIC 
 # MAGIC select *
 # MAGIC from ioc_matching_lipyeow_lim.http_big as d
-# MAGIC where d.ts::timestamp between '2012-03-16T19:39:45.300+0000' and '2012-03-16T19:43:18.080+0000'
-# MAGIC   and `id.orig_h` = '192.168.202.4' and `id.resp_h` = '192.168.21.253'
+# MAGIC where d.ts::timestamp between '2012-03-16T19:35:15.660+0000' and '2012-03-16T19:35:25.410+0000'
+# MAGIC   and `id.orig_h` = '192.168.202.88' and `id.resp_h` = '192.168.21.253'
 
 # COMMAND ----------
 
